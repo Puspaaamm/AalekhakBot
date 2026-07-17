7f723b4370af6de46e09a0632d399825d0e5ceb3
Puspaaamm <pushpam010203@gmail.com>
2026-07-17 07:31:43 +0530

    Update Wikipedia bot username in user-config.py

---
+++ user-config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+family = 'wikipedia'
+mylang = 'hi'
+
+# Replace 'YourBotUsername' with your actual registered Wikipedia Bot name
+usernames['wikipedia']['hi'] = 'Aalekhak-Bot'
+
+# Password file designation
+password_file = "user-password.py"
